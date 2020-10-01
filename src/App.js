446c75634aa6d08e322abda9aa5c6a696331fcc6
Alejandra Ossa Jimenez <alejandra.ossa.jimenez@gmail.com>
2020-10-01 19:20:09 +0200

    latest version react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from 'react';
+import "./App.css";
 
-function App() {
+
+const App = () => {
+  const [hackernews, setHackernews] = useState(null);
+  //const [searchBar, setSearchBar] = useState(" ");
+
+  useEffect(() => {
+    fetch(
+      "https://hn.algolia.com/api/v1/search_by_date?query="
+    )
+      .then((res) => res.json())
+      .then((data) => setHackernews(data));
+  }, []);
+
+  console.log(hackernews);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="body">
+        <div className="header"> 
+        <h2 > CATerNews </h2>
+        </div>
+        <div className="main">
+        <div className="searchBar">
+          <form >
+            <input type="search"></input>
+            <input type="submit" value="Search"></input>
+          </form>
+          </div>
+          {hackernews &&
+            hackernews.hits.filter((news) => news.story_url).map((news) => {
+                return (
+                  <Fragment key={news.hits}>
+                    <h2>
+                      <a className="titleStyles" href={news.story_url}>
+                        {news.story_title}
+                      </a>
+                    </h2>
+                    <p> </p>
+                    <p> author: {news.author}</p>
+                    <p>{news.created_at}</p>
+                    <br />
+                  </Fragment>
+                );
+              })}
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
